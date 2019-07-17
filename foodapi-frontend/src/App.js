0a89e47fcc8c5214031c8193beab908f1b7d68e8
crisdiaz0 <ape4p2diaz.cristian@gmail.com>
2019-07-17 10:10:09 -0500

    Created static product data to use. Changed App.js to a class component to have a local state that will keep track of the order

--- foodapi-frontend/src/App.js
+++ foodapi-frontend/src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { products } from './productData';
 
-function App() {
-	return (
-		<div className="App">
-			<h1>Food API</h1>
-		</div>
-	);
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<h1>Q.T.P</h1>
+				<h1>Quesdallias, Tacos. Provecho!</h1>
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ foodapi-frontend/src/productData.js
@@ -0,0 +1,70 @@
+/* 
+Products {
+    String id,
+    String name,
+    float price
+}
+*/
+
+export const products = [
+	{
+		id: 0,
+		name: 'Tacos Al Pastor',
+		price: 15.0
+	},
+	{
+		id: 1,
+		name: 'Tacos de Asada',
+		price: 15.0
+	},
+	{
+		id: 2,
+		name: 'Tacos de Lengua',
+		price: 15.0
+	},
+	{
+		id: 3,
+		name: 'Tacos de Carnitas',
+		price: 15.0
+	},
+	{
+		id: 4,
+		name: 'Tacos de Lengua',
+		price: 15.0
+	},
+	{
+		id: 5,
+		name: 'Tamales de Pollo',
+		price: 15.0
+	},
+	{
+		id: 6,
+		name: 'Pozole',
+		price: 15.0
+	},
+	{
+		id: 7,
+		name: 'Consome',
+		price: 15.0
+	},
+	{
+		id: 8,
+		name: 'Mole',
+		price: 15.0
+	},
+	{
+		id: 9,
+		name: 'Tortilla de Maiz',
+		price: 15.0
+	},
+	{
+		id: 10,
+		name: 'Tortilla de Harina',
+		price: 15.0
+	},
+	{
+		id: 11,
+		name: 'Quesadilla',
+		price: 15.0
+	}
+];
