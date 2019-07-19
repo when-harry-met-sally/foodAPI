3fdb3253142a5a7203ca81db629cccb4328f53f1
crisdiaz0 <ape4p2diaz.cristian@gmail.com>
2019-07-19 09:38:52 -0500

    Added Exception Handeling and validating incoming order id requests. Spereated some code to functions to improve readability and reusability

--- src/main/java/unigroup/qtp/controller/OrderController.java
+++ src/main/java/unigroup/qtp/controller/OrderController.java
@@ -1,5 +1,6 @@
 package unigroup.qtp.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import unigroup.qtp.model.Order;
@@ -9,61 +10,56 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
+@Slf4j
 public class OrderController {
     OrderRepository repository = new OrderRepository();
     @GetMapping("/orders")
-    public List<Order> getOrders(@RequestParam(value="favorite", required = false) Boolean favoriteQuery, @RequestParam(value="rating", required = false) Integer ratingQuery){
-        List<Order> orders = repository.getOrders().values().stream().collect(Collectors.toList());
-        if (favoriteQuery != null){
+    public List<Order> getOrders(@RequestParam(value="favorite", required = false) Boolean favoriteQuery,
+                                 @RequestParam(value="rating", required = false) Integer ratingQuery) {
+        log.info("Get All Orders Request Received");
+
+        List<Order> orders = repository.ordersMapToList();
+        if (favoriteQuery != null) {
             orders = orders.stream().filter(order -> order.getFavorite() == favoriteQuery).collect(Collectors.toList());
         }
-        if (ratingQuery != null){
+
+        if (ratingQuery != null) {
             orders =
                     orders.stream().filter(order -> order.getRating().equals(ratingQuery)).collect(Collectors.toList());
         }
+
         return orders;
     }
 
     @GetMapping("/orders/{id}")
-    public Order getOrder(@RequestParam(value="id") String id){
+    public Order getOrder(@RequestParam(value="id") String id) {
+        log.info("Get Orders By ID Request Received: ID {}", id);
         return repository.getOrders().get(id);
     }
 
     @PostMapping("/orders")
     public List<Order> addOrder(){
+        log.info("Add New Order Request Received");
         return repository.addOrder();
     }
 
 
     @PutMapping("/orders/{id}")
-    public List<Order> editOrder(@PathVariable String id, @RequestBody Order order){
+    public List<Order> editOrder(@PathVariable String id, @RequestBody Order order) {
+        log.info("Edit Order Request Received: ID {}", id);
         return repository.editOrder(id, order);
     }
 
-
     @DeleteMapping("/orders/{id}")
-    public List<Order> deleteOrder(@PathVariable String id){
+    public List<Order> deleteOrder(@PathVariable String id) {
+        log.info("Delete Order Request Received: ID {}", id);
         return repository.deleteOrder(id);
     }
 
-    //EXCEPTION HANDLING
-
-//    @ResponseBody
-//    @ExceptionHandler(IllegalArgumentException.class)
-//    @ResponseStatus(HttpStatus.BAD_REQUEST)
-//    public String IllegalArgumentException(IllegalArgumentException ex) {
-//        return "Illegal Argument";
-//    }
-
-//    @ResponseBody
-//    @ExceptionHandler(GameNotFoundException.class)
-//    @ResponseStatus(HttpStatus.NOT_FOUND)
-//    public String GameNotFoundException(GameNotFoundException ex) {
-//        log.warn("Game not found",ex);
-//        return "Game not found";
-//    }
-
-
-
-
+    @ResponseBody
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String IllegalArgumentException(IllegalArgumentException iae) {
+        return iae.getMessage();
+    }
 }
--- src/main/java/unigroup/qtp/repository/OrderRepository.java
+++ src/main/java/unigroup/qtp/repository/OrderRepository.java
@@ -11,27 +11,45 @@ import java.util.stream.Collectors;
 public class OrderRepository {
     private Map<String, Order> orders = new HashMap();
 
+    private void validateOrderID(String id) {
+        Order order = this.orders.get(id);
+
+        if (order == null)
+            throw new IllegalArgumentException("Could not find Order with provided ID");
+    }
+
+    public List<Order> ordersMapToList() {
+        return this.orders.values().stream().collect(Collectors.toList());
+    }
+
     public List<Order> addOrder(){
         Order order = new Order();
         this.orders.put(order.getId(), order);
-        return this.orders.values().stream().collect(Collectors.toList());
+        return this.ordersMapToList();
     }
 
     public List<Order> editOrder(String id, Order edit){
-        orders.put(id, edit);
-        edit.setPrice(calculateTotalPrice(edit));
-        return this.orders.values().stream().collect(Collectors.toList());
+        this.validateOrderID(id);
+
+        edit.setPrice(this.calculateTotalPrice(edit));
+        this.orders.put(id, edit);
+
+        return this.ordersMapToList();
     }
 
     public List<Order> deleteOrder(String id){
+        this.validateOrderID(id);
+
         orders.remove(id);
-        return this.orders.values().stream().collect(Collectors.toList());
+        return this.ordersMapToList();
     }
-    public float calculateTotalPrice(Order order){
+
+    private float calculateTotalPrice(Order order){
         float total = 0;
         for (Product product: order.getCart()){
             total += product.getPrice();
         }
+
         return total;
     }
 }
